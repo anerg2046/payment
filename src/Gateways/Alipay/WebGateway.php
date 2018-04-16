42c33b0385d7802c7a8f85382d2a7755faf39c37
Coeus <coeus@CoeusdeMacBook-Pro.local>
2018-04-16 18:48:37 +0800

    修改注释

--- src/Gateways/Alipay/WebGateway.php
+++ src/Gateways/Alipay/WebGateway.php
@@ -11,7 +11,7 @@ class WebGateway extends _Gateway
     {
         //设定交易接口名称
         Datasheet::set('method', 'alipay.trade.page.pay');
-        //设定手机支付的特定参数
+        //设定网站支付的特定参数
         BizData::set('product_code', 'FAST_INSTANT_TRADE_PAY');
 
         return $this->buildPayHtml();
