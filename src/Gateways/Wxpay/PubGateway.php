614920d43d91b2ca49ba0e540b428732f7d898be
Coeus <coeus@CoeusdeMacBook-Pro.local>
2018-05-15 12:04:15 +0800

    公众号支付参数

--- src/Gateways/Wxpay/PubGateway.php
+++ src/Gateways/Wxpay/PubGateway.php
@@ -10,18 +10,18 @@ class PubGateway extends _Gateway
 
     public function pay()
     {
-		//设定交易方式，并执行统一下单
+        //设定交易方式，并执行统一下单
         Datasheet::set('trade_type', 'JSAPI');
-		$ret = $this->unifiedorder();
-		//拼接公众号支付的javascript参数
+        $ret = $this->unifiedorder();
+        //拼接公众号支付的javascript参数
         $jsParams = [
-            'appId' => Datasheet::get('app_id'),
+            'appId'     => Datasheet::get('app_id'),
             'timeStamp' => time() . '',
-            'nonceStr' => Str::random(),
-            'package' => 'prepay_id=' . $ret['prepay_id'],
-            'signType' => 'MD5',
+            'nonceStr'  => Str::random(),
+            'package'   => 'prepay_id=' . $ret['prepay_id'],
+            'signType'  => 'MD5',
         ];
-		$jsParams['sign'] = Helper::signature($jsParams, Datasheet::get('md5_key'));
-		return $jsParams;
+        $jsParams['paySign'] = Helper::signature($jsParams, Datasheet::get('md5_key'));
+        return $jsParams;
     }
 }
